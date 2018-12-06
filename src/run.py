8b6c33ea281f1fde632ce382b2bc8479657004fa
Vadim <monstarnn@gmail.com>
2018-12-06 18:11:10 +0300

    prepare for export

--- src/run.py
+++ src/run.py
@@ -23,6 +23,7 @@ def main():
     parser.add_argument('--only_train', default='False')
     parser.add_argument('--model_name')
     parser.add_argument('--model_version')
+    parser.add_argument('--export', action='store_true', help='Export model')
     args, _ = parser.parse_known_args()
 
     with open('faster_rcnn.config', 'r') as cf:
@@ -50,7 +51,12 @@ def main():
     train_steps = train_and_eval_dict['train_steps']
     eval_input_fns = train_and_eval_dict['eval_input_fns']
     if args.evaluator:
-        continuous_eval(estimator, model_dir, eval_input_fns[0], 'validation_data')
+        model_name = None
+        model_version = None
+        if args.export:
+            model_name = args.model_name
+            model_version = args.model_version
+        continuous_eval(estimator, model_dir, eval_input_fns[0], 'validation_data', model_name, model_version)
     elif os.environ.get("TF_CONFIG", '') != '':
         eval_on_train_input_fn = train_and_eval_dict['eval_on_train_input_fn']
         predict_input_fn = train_and_eval_dict['predict_input_fn']
@@ -66,7 +72,7 @@ def main():
         estimator.train(input_fn=train_input_fn, max_steps=train_steps)
 
 
-def continuous_eval(estimator, model_dir, input_fn, name):
+def continuous_eval(estimator, model_dir, input_fn, name, model_name=None, model_version=None):
     def terminate_eval():
         tf.logging.warning('Eval timeout after 180 seconds of no checkpoints')
         return False
@@ -76,15 +82,23 @@ def continuous_eval(estimator, model_dir, input_fn, name):
             timeout_fn=terminate_eval):
 
         tf.logging.info('Starting Evaluation.')
+        loss = None
         try:
             eval_results = estimator.evaluate(
                 input_fn=input_fn, steps=None, checkpoint_path=ckpt, name=name)
             ##names = ['DetectionBoxes_Precision/mAP','DetectionBoxes_Precision/mAP (large)','']
             res = {}
-            for k,v in eval_results.items():
+            for k, v in eval_results.items():
                 if isinstance(v, numbers.Number):
                     res[k] = v
-            tf.logging.info('Eval results: {}'.format(v))
+                if k == 'Loss/total_loss':
+                    tf.logging.info('Previous loss: {}, current: {}'.format(loss, v))
+                    if loss is None or loss < v:
+                        if model_name is not None and model_version is not None:
+                            tf.logging.info('Starting export to model {}:{}'.format(model_name, model_version))
+                        else:
+                            tf.logging.info('Skipping model export')
+                tf.logging.info('Eval results: {}'.format(v))
 
             # Terminate eval job when final checkpoint is reached
             current_step = int(os.path.basename(ckpt).split('-')[1])
