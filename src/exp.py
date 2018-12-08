862d372069cab23d032f5b5e7e07fa0bf8d5fccb
Vadim <monstarnn@gmail.com>
2018-12-08 16:42:39 +0300

    try to export separately

---
+++ src/exp.py
@@ -0,0 +1,13 @@
+from run import export_subprocess
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('--training_dir')
+parser.add_argument('--research_dir')
+parser.add_argument('--model_name', default="object-detection")
+parser.add_argument('--model_version', default="1.0.0")
+parser.add_argument('--train_build_id')
+parser.add_argument('--train_checkpoint')
+args, _ = parser.parse_known_args()
+
+export_subprocess(args.research_dir, args.training_dir, args.train_build_id, args.train_checkpoint, args.model_name, args.model_version)
